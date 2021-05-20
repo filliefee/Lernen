cd9594e39a506e7f7c67827e7ef2eccdaadcdd3a
filliefee <filliefee@googlemail.com>
2021-05-20 09:12:37 +0200

    Summieren mit Ganzahlen möglich

---
+++ Lernen/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+//using System.Linq;
+
+namespace Lernen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Gib den ersten Summanden ein:");
+            string ersterSummand = Console.ReadLine();
+            Console.Write("Gib den zweiten Summanden ein:");
+            string zweiterSummand = Console.ReadLine();
+
+            //Wandlung von string in int
+            int ersterSummandZahl = Convert.ToInt32(ersterSummand);
+            int zweiterSummandZahl = Convert.ToInt32(zweiterSummand);
+
+            Console.WriteLine("Die Summe ist: {0}",ersterSummandZahl + zweiterSummandZahl) ;
+            Console.ReadKey();
+        }
+    }
+}
